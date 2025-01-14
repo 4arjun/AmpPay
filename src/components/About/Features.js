634e12bd99d4879bc7aa7e06340bb43c539e0306
4arjun <arjunajith440@gmail.com>
2025-01-14 17:40:29 +0530

    fix icon error

--- src/components/About/Features.js
+++ src/components/About/Features.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./Features.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Features = () => {
   useEffect(() => {
